30c58dccb566ca5cc729deeb46988a4239cc765a
Miroslav-bit <suinfotrg@gmail.com>
2025-07-12 13:43:00 +0200

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const postSchema = new mongoose.Schema({
   text: String,
   date: { type: Date, default: Date.now },
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User'
-  }
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  panelOwnerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  mainCategory: String,
+  subCategory: String
 });
 
 module.exports = mongoose.model('Post', postSchema);
