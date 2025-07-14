cf1a042e22bcf591052ef382b56b505fd99f7de8
Miroslav-bit <suinfotrg@gmail.com>
2025-07-14 18:48:54 +0200

    Update server.js

--- server.js
+++ server.js
@@ -24,5 +24,3 @@ app.use('/api/user', userRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server aktivan na portu ${PORT}`));
-
-app.use('/api/ratings', require('./routes/ratings'));
