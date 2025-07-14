1e0091cf006a994c405c8e5c5a48ef3e90673176
Miroslav-bit <suinfotrg@gmail.com>
2025-07-14 15:34:28 +0200

    Update ratings.js

--- models/ratings.js
+++ models/ratings.js
@@ -1,36 +1,22 @@
-const express = require('express');
-const router = express.Router();
-const Rating = require('../models/Rating');
+const mongoose = require('mongoose');
 
-// POST /api/ratings/save
-router.post('/save', async (req, res) => {
-  const { cardName, cardSub, rater, score } = req.body;
-
-  try {
-    let rating = await Rating.findOne({ cardName, cardSub, rater });
-    if (rating) {
-      rating.score = score;
-      await rating.save();
-    } else {
-      rating = new Rating({ cardName, cardSub, rater, score });
-      await rating.save();
-    }
-    res.json({ msg: 'Ocena sačuvana', rating });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Greška pri upisu ocene u bazu' });
+const RatingSchema = new mongoose.Schema({
+  cardName: {
+    type: String,
+    required: true
+  },
+  cardSub: {
+    type: String,
+    required: true
+  },
+  rater: {
+    type: String,
+    required: true
+  },
+  score: {
+    type: Number,
+    required: true
   }
-});
+}, { timestamps: true });
 
-router.get('/list', async (req, res) => {
-  const { cardName, cardSub } = req.query;
-
-  try {
-    const ratings = await Rating.find({ cardName, cardSub });
-    res.json(ratings);
-  } catch (err) {
-    res.status(500).json({ msg: 'Greška pri čitanju iz baze' });
-  }
-});
-
-module.exports = router;
+module.exports = mongoose.model('Rating', RatingSchema);
