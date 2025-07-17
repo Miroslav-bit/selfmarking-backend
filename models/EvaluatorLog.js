49e22e47f66fd1fff40d4cc80382e1927db6a628
Miroslav-bit <suinfotrg@gmail.com>
2025-07-17 11:26:24 +0200

    Delete models/EvaluatorLog.js

--- models/EvaluatorLog.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const EvaluatorLogSchema = new mongoose.Schema({
-  rater: { type: String, required: true },
-  evaluated: { type: [String], default: [] }
-});
-
-module.exports = mongoose.model('EvaluatorLog', EvaluatorLogSchema);
