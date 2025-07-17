ab72b4871940bc226225107af3068acf74ba6480
Miroslav-bit <suinfotrg@gmail.com>
2025-07-17 08:54:49 +0200

    Update EvaluatorLog.js

--- models/EvaluatorLog.js
+++ models/EvaluatorLog.js
@@ -1,48 +1,8 @@
-const express = require('express');
-const router = express.Router();
-const Rating = require('../models/Rating');
+const mongoose = require('mongoose');
 
-// POST /api/ratings/save
-router.post('/save', async (req, res) => {
-  const { cardName, cardSub, rater, score } = req.body;
-
-  try {
-    let rating = await Rating.findOne({ cardName, cardSub, rater });
-    if (rating) {
-      rating.score = score;
-      await rating.save();
-    } else {
-      rating = new Rating({ cardName, cardSub, rater, score });
-      await rating.save();
-    }
-    res.json({ msg: 'Ocena sačuvana', rating });
-  } catch (err) {
-    console.error(err);
-    res.status(500).json({ msg: 'Greška pri upisu ocene u bazu' });
-  }
+const EvaluatorLogSchema = new mongoose.Schema({
+  rater: { type: String, required: true },
+  evaluated: { type: [String], default: [] }
 });
 
-router.get('/list', async (req, res) => {
-  const { cardName, cardSub } = req.query;
-
-  try {
-    const ratings = await Rating.find({ cardName, cardSub });
-    res.json(ratings);
-  } catch (err) {
-    res.status(500).json({ msg: 'Greška pri čitanju iz baze' });
-  }
-});
-
-// GET ocenjivači za konkretnu temu
-router.get('/raters', async (req, res) => {
-  try {
-    const { cardName, cardSub } = req.query;
-    const ratings = await Rating.find({ cardName, cardSub }).select('rater score -_id');
-    res.json(ratings);
-  } catch (err) {
-    console.error("Greška pri dohvatanju ocenjivača:", err);
-    res.status(500).json({ msg: "Greška na serveru." });
-  }
-});
-
-module.exports = router;
+module.exports = mongoose.model('EvaluatorLog', EvaluatorLogSchema);
