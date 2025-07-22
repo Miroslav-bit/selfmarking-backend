3196b199e1e9b6c2f14f5e43fcc546869a3416c2
Miroslav-bit <suinfotrg@gmail.com>
2025-07-22 19:03:06 +0200

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -3,12 +3,14 @@ const mongoose = require('mongoose');
 const PostSchema = new mongoose.Schema({
   text: { type: String, required: true },
   imageUrl: { type: String },
-  videoUrl: { type: String }, // ← NOVO
+  videoUrl: { type: String },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   panelOwnerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   mainCategory: { type: String, required: true },
   subCategory: { type: String, required: true },
   date: { type: Date, default: Date.now }
+  confirmators: [{ type: String }],
+  deniers: [{ type: String }]
 });
 
 module.exports = mongoose.model('Post', PostSchema);
