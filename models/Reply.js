cb6282d5e4e35350726ad9420f7c1c6e4d5e635f
Miroslav-bit <suinfotrg@gmail.com>
2025-07-26 13:26:37 +0200

    Update Reply.js

--- models/Reply.js
+++ models/Reply.js
@@ -5,11 +5,7 @@ const ReplySchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text: { type: String, required: true },
   date: { type: Date, default: Date.now }
+  isHidden: { type: Boolean, default: false }
 });
-
-isHidden: {
-  type: Boolean,
-  default: false
-}
 
 module.exports = mongoose.model('Reply', ReplySchema);
