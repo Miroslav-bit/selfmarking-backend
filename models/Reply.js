70f17bb2e8007f258cb4ec9a81c6d91812f8a7ea
Miroslav-bit <suinfotrg@gmail.com>
2025-07-26 13:43:09 +0200

    Update Reply.js

--- models/Reply.js
+++ models/Reply.js
@@ -4,7 +4,7 @@ const ReplySchema = new mongoose.Schema({
   postId: { type: mongoose.Schema.Types.ObjectId, ref: 'Post', required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
   text: { type: String, required: true },
-  date: { type: Date, default: Date.now }
+  date: { type: Date, default: Date.now },
   isHidden: { type: Boolean, default: false }
 });
 
