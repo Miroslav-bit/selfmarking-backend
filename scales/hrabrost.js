854df37336862e831b212032cb398815151ea5f2
Miroslav-bit <suinfotrg@gmail.com>
2025-07-29 17:14:38 +0200

    Add files via upload

---
+++ scales/hrabrost.js
@@ -0,0 +1,17 @@
+module.exports = `+1000 = veliki herojski podvig značajan za svet i čovečanstvo
++750 = herojski podvig na nivou nacije
++500 = herojski podvig na nivou lokalne sredine ili porodice
++300 = herojski podvig za druge pojedince
++200 = prevazilaženje fobije ili probijanje ličnih granica
++100 = natprosečni dokaz hrabrosti
++50 = prosečan dokaz hrabrosti
++10 = mali znak hrabrosti
+-10 = mali znak kukavičluka
+-50 = prosečan znak kukavičluka
+-100 = natprosečan znak kukavičluka
+-250 = kukavičluk koji je štetan za druge
+-500 = kukavičluk sa fobijama i paranojom
+-750 = patološki kukavičluk
+-1000 = ekstremni kukavičluk
+
+Ako objava nema veze sa hrabrošću osobe {{IME}}, dodeli 0 bodova.`;
